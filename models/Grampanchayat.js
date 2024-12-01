8d0b882ccbb1292007c190b61e46c29b836a9eb8
ashikasoni5404 <ashikasoni5858@gmail.com>
2024-12-02 02:57:21 +0530

    api

--- models/Grampanchayat.js
+++ models/Grampanchayat.js
@@ -7,29 +7,20 @@ const grampanchayatSchema = new mongoose.Schema({
   grampanchayatId: { type: String, required: true, unique: true },
   address: { type: String, required: true },
   villageName: { type: String, required: true },
-  password: { type: String, required: true}, 
+  city: { type: String, required: true },
+  district: { type: String, required: true },
+  state: { type: String, required: true },
+  pincode: { type: String, required: true },
+  password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  mobile: { 
+    type: String, 
+    required: true, 
+    match: [/^\d{10}$/, 'Mobile number must be 10 digits'], 
+    unique: true 
+  },
   status: { type: Number, default: 1 },
 });
-
-// Password hashing before saving Grampanchayat
-grampanchayatSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
-    // Hash the password before saving it
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
-
-// Ensure `updatedAt` is updated on updates
-grampanchayatSchema.pre('findOneAndUpdate', function(next) {
-  this.set({ updatedAt: new Date() });
-  next();
-});
-grampanchayatSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id, grampanchayatId: this.grampanchayatId }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  return token;
-};
 
 module.exports = mongoose.model('Grampanchayat', grampanchayatSchema);
